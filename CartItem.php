28732ed8cb8aba788108489d40ba4d5d67adaa30
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2022-11-28 10:54:49 +0100

    Product

---
+++ CartItem.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class CartItem
+{
+    private $product;
+    private int $quantity;
+
+    // TODO Skriv en konstruktor som sätter alla properties
+
+    // public function __construct($id, $title, $price, $inStock);
+    // {
+    //     $this -> $id = id; 
+    //     $this -> $title = title;
+    //     $this -> $price = price;
+    //     $this -> $instock = instock;
+    // };
+        
+    
+    // TODO Skriv getters för alla properties
+    
+    // public function getProduct() {
+    //     return $this -> $id;
+    // }
+
+    // public function getProduct() {
+    //     return $this -> $title;
+    // }
+
+    // public function getProduct
+
+
+
+    //VG: Skall utöka antalet på ett cartItem med 1. 
+    //VG: Det skall inte vara möjligt att utöka så att antalet överstiger produktens $inStock.
+    //public function increaseQuantity()
+    
+}
