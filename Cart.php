2ed5839c2ad8233c102d803169ca146d9c689433
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2022-11-29 15:27:23 +0100

    Some more changes

--- Cart.php
+++ Cart.php
@@ -4,53 +4,67 @@
 class Cart
 {
     private array $items = [];
-
-
+    
+    
+    
     //TODO Skriv getter för items
     
     public function getItems() {
-    return $this -> items;
+        return $this -> items;
     }
-
+    
     /*
-     Skall lägga till en produkt i kundvagnen genom att
-     skapa ett nytt cartItem och lägga till i $items array.
-     Metoden skall returnera detta cartItem.
-
-     VG: Om produkten redan finns i kundvagnen
-     skall istället quantity på cartitem ökas.
-     */
-
+    Skall lägga till en produkt i kundvagnen genom att
+    skapa ett nytt cartItem och lägga till i $items array.
+    Metoden skall returnera detta cartItem.
+    
+    VG: Om produkten redan finns i kundvagnen
+    skall istället quantity på cartitem ökas.
+    */
+    
     public function addProduct($product)
     {
-        $cartItem = new cartItem ($product, 1);
-        array_push($this -> items, $cartItem);
-        return $cartItem;
+        
+        
+        $cartItems = new cartItem ($product, 1);
+        array_push($this -> items, $cartItems);
+        return $cartItems;
     }
-
-
-
+    
+    
+    
     //Skall ta bort en produkt ur kundvagnen (använd unset())
     public function removeProduct($product)
     {
-    
-    
+        
+        unset($this -> items[0]);
+        return $this -> items;
     }
-
+    
     //Skall returnera totala antalet produkter i kundvagnen
     //OBS: Ej antalet unika produkter
     public function getTotalQuantity()
     {
-        $totQuantity = count($this -> items);
-            return $totQuantity;
-       
+        $totQuantity = 0;
         
+        foreach($this -> items as $item) {
+            $totQuantity += $item -> getQuantity();
+        
+        }
+        return $totQuantity;
     }
-
+    
     //Skall räkna ihop totalsumman för alla produkter i kundvagnen
     //VG: Tänk på att ett cartitem kan ha olika quantity
-    public function getTotalSum()
-    {
+    public function getTotalSum() {
         
+        $totSum = 0;
+        
+        foreach($this -> items as $item) {
+            $totSum += $item -> getProduct() -> getPrice();
+        
+        }
+        return $totSum;
     }
-}
+    
+     }
--- CartItem.php
+++ CartItem.php
@@ -4,13 +4,14 @@
 class CartItem
 {
     private $product;
-    private int $quantity;
+    private int $quantity;  
     
+
     // TODO Skriv en konstruktor som sätter alla properties
 
     public function __construct($product, $quantity)
     {
-        $this -> product = $product; 
+        $this -> product = $product;
         $this -> quantity = $quantity;
     }
         
@@ -28,8 +29,10 @@ class CartItem
     //VG: Skall utöka antalet på ett cartItem med 1. 
     //VG: Det skall inte vara möjligt att utöka så att antalet överstiger produktens $inStock.
     public function increaseQuantity()
-    {
+    {   
         
-    }
+     
 
+    
+}
 }
